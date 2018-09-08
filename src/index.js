e305c86733c4de18275df0e5e72313d21527b9e7
jticer4 <jticer@cnm.edu>
2018-09-08 14:40:20 -0600

    Applied the grid style to the grid and set the width of the grid inline. I also created a for loop tht will check to see if the box is on or off.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 
 class Grid extends React.Component {
 	render() {
+		const width = this.props.cols * 14;
+		var rowsArr = [];
+
+		var boxClass = "";
+		for (var i = 0; i < this.props.rows; i++) {
+			for (var j = 0; j < this.props.cols; j++){
+				let boxId = i + "_" + j;
+
+				boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+			}
+		}
+
 		return (
-			<div>
-				Grid!
+			<div className="grid" style={{width: width}}>
+				{{rowsArr}}
 			</div>
 		)
 	}
